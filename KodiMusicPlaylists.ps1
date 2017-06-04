c6b1a664e59585e4154cfab20f3a9eaaebefcc2e
David Kennedy <davidken@live.ie>
2017-06-04 19:19:19 +0100

    Initial Version

--- KodiMusicPlaylists.ps1
+++ KodiMusicPlaylists.ps1
@@ -1,10 +1,16 @@
-#
-# KodiMusicPlaylists.ps1
-#
+<#  
+.SYNOPSIS  
+    Generate Kody playlists  
+.DESCRIPTION  
+    This script generates Kodi playlists and writes them 
+	directly to the playlist folder in Kodi
+.LINK  
+#>
+
 Set-ExecutionPolicy RemoteSigned
 Add-Type –Path 'C:\Program Files (x86)\MySQL\MySQL Connector Net 6.9.8\Assemblies\v4.5\MySql.Data.dll'
 
-$Connection = [MySql.Data.MySqlClient.MySqlConnection]@{ConnectionString='server=10.10.1.100;uid=root;pwd=pa5CAL01D;database=mymusic56'}
+$Connection = [MySql.Data.MySqlClient.MySqlConnection]@{ConnectionString='server=lattuce-dc.lattuce.com;uid=root;pwd=pa5CAL01D;database=mymusic56'}
 $OutputFile = "\\10.10.1.24\Userdata\playlists\music\Music Added in 2017.m3u"
 
 $Connection.Open()
