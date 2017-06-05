674da702eb18c1f84c67fa4e6d6736c92f7e1f01
David Kennedy <davidken@live.ie>
2017-06-05 13:47:59 +0100

    Code tidyup

--- KodiMusicPlaylists.ps1
+++ KodiMusicPlaylists.ps1
@@ -13,23 +13,22 @@ Param(
 $Cred1="eee"
 Add-Type –Path 'C:\Program Files (x86)\MySQL\MySQL Connector Net 6.9.8\Assemblies\v4.5\MySql.Data.dll'
 $Year=get-date -Format yyyy
-$Connection = [MySql.Data.MySqlClient.MySqlConnection]@{ConnectionString='server=lattuce-dc.lattuce.com;uid=root;pwd=$Cred;database=mymusic56'}
+$Connection = [MySql.Data.MySqlClient.MySqlConnection]@{ConnectionString=''}
 $Connection.ConnectionString="server=lattuce-dc.lattuce.com;uid=root;pwd=$Cred;database=mymusic56"
 $OutputFile = "\\10.10.1.24\Userdata\playlists\music\Music Added in $($Year).m3u"
-Write-output $OutputFile
+
 $Connection.Open()
 $MYSQLCommand = New-Object MySql.Data.MySqlClient.MySqlCommand
 $MYSQLDataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter
 $MYSQLDataSet = New-Object System.Data.DataSet
 $MYSQLCommand.Connection=$Connection
 $Sql = 'SELECT "#EXTM3U"
- UNION ALL
- SELECT CONCAT ("#EXTINF:", iDuration, ", ", strArtists, " - ", strTitle, char(13),char(10), p.strPath, s.strFileName )   
-FROM mymusic56.song s, mymusic56.path p
-where s.dateAdded is not null
-and s.dateAdded >= STR_TO_DATE(''01,01,' + $($Year) + ''',''%d,%m,%Y'')
-and s.dateAdded >= STR_TO_DATE(''01,01,' + $($Year-1) + ''',''%d,%m,%Y'')
-and s.idPath = p.idPath;'
+		UNION ALL
+		SELECT CONCAT ("#EXTINF:", iDuration, ", ", strArtists, " - ", strTitle, char(13), char(10), p.strPath, s.strFileName )   
+		FROM mymusic56.song s, mymusic56.path p
+		where s.dateAdded is not null
+		and s.dateAdded >= STR_TO_DATE(''01,01,' + $($Year) + ''',''%d,%m,%Y'')
+		and s.idPath = p.idPath;'
 
 $MYSQLCommand.CommandText = $Sql
 $MYSQLDataAdapter.SelectCommand=$MYSQLCommand
