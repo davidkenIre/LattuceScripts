cd1518d449994c78bc223f75086d4dbe9e8957fc
David Kennedy <davidken@live.ie>
2018-03-03 21:01:22 +0000

    Create a general playlist based on a list of Genres

--- Python/GooglePlayPlaylists.py
+++ Python/GooglePlayPlaylists.py
@@ -1,7 +1,8 @@
-
 # Docs
 # https://unofficial-google-music-api.readthedocs.io/en/latest/
 
+# TODO
+# 1. Need to limit the number of songs added to a playlist to 1000
 
 # Dependancies
 # pip install gmusicapi
@@ -19,7 +20,7 @@ password = QueryValueEx(hKey, "GooglePassword")[0]
 api = Mobileclient()
 api.login(username, password, Mobileclient.FROM_MAC_ADDRESS) # => True
 
-
+# Reactivate
 # Delete Playlist
 playlists = api.get_all_playlists()
 for playlist in playlists:
@@ -27,13 +28,31 @@ for playlist in playlists:
 		id_to_delete = playlist['id']
 		print('Deleting Playlist ID: ', id_to_delete)
 		api.delete_playlist(id_to_delete)
-
-# Reactivate
+# Add new Playlist
 playlist_id = api.create_playlist('Reactivate')
 songs = api.get_all_songs()
 playlistsongs=[]
 for song in songs:
 	if 'Reactivate' in song['album']:
 		print('Adding song ID: ', song['id'], 'to playlist Reactivate')		
+		playlistsongs.append(song['id'])
+api.add_songs_to_playlist(playlist_id, playlistsongs)
+
+# Dance etc.
+# Delete Playlist
+playlists = api.get_all_playlists()
+for playlist in playlists:
+	if playlist['name']=='Dance':
+		id_to_delete = playlist['id']
+		print('Deleting Playlist ID: ', id_to_delete)
+		api.delete_playlist(id_to_delete)
+# Add new Playlist
+playlist_id = api.create_playlist('Dance')
+songs = api.get_all_songs()
+playlistsongs=[]
+
+for song in songs:
+	if 'Dance' == song['genre'] or 'Big Room' == song['genre']  or 'Dance' == song['genre'] or 'Drum & Bass' == song['genre'] or 'Electronic' == song['genre'] or 'Electronica' == song['genre'] or 'Hard Dance' == song['genre'] or 'Hardcore' == song['genre'] or 'House' == song['genre'] or 'Progressive House' == song['genre'] or 'Psy-Trance' == song['genre'] or 'Rave' == song['genre'] or 'Tech House' == song['genre'] or 'Techno' == song['genre'] or 'Trance' == song['genre']:
+		print('Adding song ID: ', song['id'], 'to playlist Dance')		
 		playlistsongs.append(song['id'])
 api.add_songs_to_playlist(playlist_id, playlistsongs)
